36acf8133e477cc19a3c2311bc738f186c8c395e
Enrique Munoz <enrique.apple.id@gmail.com>
2021-03-14 00:13:39 -0800

    up to SetGameControllerReference() in GridSpace

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
+
+    private GameController gameController;
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = playerSide;
+        button.interactable = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
